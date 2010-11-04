94633f6e8f5985ea5549547fe708296dd658f42a
msftguy <msftguy@MyBook.local>
2010-11-04 00:29:47 -0700

    Initial Commit

---
+++ Source/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  genpass
+//
+//  Created by msftguy on 11/4/10.
+//  Copyright (c) 2010 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[]) {
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
